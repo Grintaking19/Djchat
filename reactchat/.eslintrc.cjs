7e668a8b919232c74c874e3744fcfef11a239977
Omar Badr <omarbadrh.ob@gmail.com>
2023-08-25 01:38:10 +0200

    :art: Update project structure for frontend

--- reactchat/.eslintrc.cjs
+++ reactchat/.eslintrc.cjs
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './reactchat/tsconfig.json',
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 0,
   },
-}
+};
--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,12 +1,10 @@
-
-
 function App() {
-
   return (
     <>
-      
+      <h1>React Chat</h1>
+      <p>Coming soon...</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
