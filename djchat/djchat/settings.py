8dda670915cef354c55989b6fc6f1a630d62f2f0
Omar Badr <omarbadrh.ob@gmail.com>
2023-08-29 18:37:06 +0200

    :sparkles: Added useCRUD hook and CORS-HEADERS

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "drf_spectacular_sidecar",  # required for Django collectstatic discovery
+    "corsheaders",
     # Internal apps
     "server",
     "account",
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -170,4 +172,8 @@ MEDIA_URL = "media/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
